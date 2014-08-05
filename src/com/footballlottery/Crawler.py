14b45075d87c699174d696334c2d329837e417ef
tiancaifjx <tiancaifjx@163.com>
2014-08-05 22:25:43 +0800

    初步解析

--- src/com/footballlottery/Crawler.py
+++ src/com/footballlottery/Crawler.py
@@ -6,7 +6,10 @@ Created on 2014年8月3日
 
 from bs4 import BeautifulSoup
 import urllib.request
+import json
+import time
 import re
+from string import Template
 
 class MyClass(object):
     '''
@@ -18,17 +21,29 @@ class MyClass(object):
         '''
         Constructor
         '''
+#----------------------------------------------乐彩数据采集网址-----------------------------------------------
+# http://www.lecai.com/include/static/odds_jczq/80/euro/jczq_20140805.json
+# http://www.lecai.com/include/static/odds_jczq/80/vs_20140805.json
+# http://data.lecai.com/football/match/oddsEuro.do?id=1260455&action=oddsEuroDetail&bookmakerId=40&isEx=0
+# http://data.lecai.com/football/match/oddsEuro.do?id=1260455
+# f= urllib.request.urlopen("http://www.lecai.com/lottery/jczq/")
+#--------------------------------------------------------------------------------------------------------
 
-f= urllib.request.urlopen("http://www.lecai.com/lottery/jczq/")
+doc = urllib.request.urlopen("http://www.lecai.com/include/static/odds_jczq/80/euro/jczq_20140805.json")
 
-doc = f.read()
+jsonstr = BeautifulSoup(doc)
 
-soup = BeautifulSoup(doc)
+print(jsonstr)
 
-#print(soup)
+s = json.loads(str(jsonstr))
 
-#a = soup.find_all('id','m_2014080301014');
-a = soup.findAll('td')
+# print(s.keys())
+# print(len(s.keys()))
 
-print(a)
+for key in s:
+    print(key)
+#   link = "http://data.lecai.com/football/match/oddsEuro.do?id="+key
+    
+
+
 
