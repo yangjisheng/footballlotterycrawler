cabe0120a553bc026394f6cfdafe971abc041c83
tiancaifjx <tiancaifjx@163.com>
2014-08-06 00:27:23 +0800

    将要取得欧赔指数

--- src/com/footballlottery/Crawler.py
+++ src/com/footballlottery/Crawler.py
@@ -26,6 +26,7 @@ class MyClass(object):
 # http://www.lecai.com/include/static/odds_jczq/80/vs_20140805.json
 # http://data.lecai.com/football/match/oddsEuro.do?id=1260455&action=oddsEuroDetail&bookmakerId=40&isEx=0
 # http://data.lecai.com/football/match/oddsEuro.do?id=1260455
+#http://data.lecai.com/football/match/oddsEuro.do?id=1252361&action=widget&page=1&size=303&filter= 百家欧赔的json串
 # f= urllib.request.urlopen("http://www.lecai.com/lottery/jczq/")
 #--------------------------------------------------------------------------------------------------------
 
@@ -33,7 +34,7 @@ doc = urllib.request.urlopen("http://www.lecai.com/include/static/odds_jczq/80/e
 
 jsonstr = BeautifulSoup(doc)
 
-print(jsonstr)
+# print(jsonstr)
 
 s = json.loads(str(jsonstr))
 
@@ -41,9 +42,16 @@ s = json.loads(str(jsonstr))
 # print(len(s.keys()))
 
 for key in s:
-    print(key)
-#   link = "http://data.lecai.com/football/match/oddsEuro.do?id="+key
+    link = "http://data.lecai.com/football/match/oddsEuro.do?id="+key
+    d = urllib.request.urlopen(link)
+    soup = BeautifulSoup(d.read())
+    totalstr=soup.find(class_="info_box")
+    #将欧赔公司总数正则出来 例： 共【306】家欧赔公司 取306 放入百家欧赔json串链接中
+    pattern = re.compile(r'【\d*】')
     
+
+
+   
 
 
 
